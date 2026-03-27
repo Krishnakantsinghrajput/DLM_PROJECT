548588e018ad025aae19b0bd66b8caf086da38df
mahakgupta2 <mahakgupta956@gmail.com>
2026-03-27 14:57:21 +0530

    server.js

--- server.js
+++ server.js
@@ -1,137 +1,104 @@
-// const express = require("express");
-
+// const express = require("express"); 
 // const {
-//     lock,
-//     unlock,
-//     showLocks
+// lock,
+// unlock, 
+// showLocks 
 // } = require("./lockManager");
+//  const app = express(); 
+//  app.use(express.json());
+//  app.use(express.urlencoded({ extended: true })); 
 
-// const app = express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-
-// // test
+// // test 
 // app.get("/", (req, res) => {
-//     res.send("Server running");
-// });
-
+//  res.send("Server running"); 
+// }); 
 
 // // LOCK
-// app.get("/lock", (req, res) => {
+// app.get("/lock", (req, res) => { 
+//const resource = req.query.resource; 
+//if (!resource) { 
+//return res.send("No resource"); 
+//} 
+// if (lock(resource)) { 
+//res.send("LOCKED"); 
+//} else { 
+// res.send("BUSY");
+//}
+//  }); 
 
-//     const resource = req.query.resource;
+// // UNLOCK 
+// app.get("/unlock", (req, res) => { 
+// const resource = req.query.resource; 
+//  if (!resource) { 
+//  return res.send("No resource"); 
+// } 
+// unlock(resource); 
+// res.send("UNLOCKED"); 
+//});
 
-//     if (!resource) {
-//         return res.send("No resource");
-//     }
+// // SHOW LOCKS 
+// app.get("/locks", (req, res) => { 
+//res.send(showLocks()); // }); 
+// app.listen(3000, () => { 
 
-//     if (lock(resource)) {
-//         res.send("LOCKED");
-//     } else {
-//         res.send("BUSY");
-//     }
-
+// console.log("Server started on port 3000");
 // });
 
-
-// // UNLOCK
-// app.get("/unlock", (req, res) => {
-
-//     const resource = req.query.resource;
-
-//     if (!resource) {
-//         return res.send("No resource");
-//     }
-
-//     unlock(resource);
-
-//     res.send("UNLOCKED");
-
-// });
-
-
-// // SHOW LOCKS
-// app.get("/locks", (req, res) => {
-
-//     res.send(showLocks());
-
-// });
-
-
-// app.listen(3000, () => {
-//     console.log("Server started on port 3000");
-// });
-
-const express = require("express");
-
+const mongoose = require("mongoose"); 
+const express = require("express"); 
 const {
-    lock,
+    lock, 
     unlock,
-    showLocks,
-    showQueue
-} = require("./lockManager");
+    showLocks, 
+    showQueue 
 
+} = require("./lockManager");
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.static("public")); 
 
 
-app.get("/", (req, res) => {
-    res.send("Distributed Lock Manager Running");
-});
+// app.use(express.json()); 
+// app.use(express.urlencoded({ extended: true })); 
 
+app.get("/", (req, res) => 
+    { res.send("Distributed Lock Manager Running"); 
 
-// LOCK
-app.get("/lock", (req, res) => {
+}); 
 
-    const resource = req.query.resource;
-    const user = req.query.user;
+// LOCK 
+app.get("/lock", (req, res) =>
+     { const resource = req.query.resource; 
+const user = req.query.user;
+ if (!resource || !user) { return res.send("Need resource & user");
 
-    if (!resource || !user) {
-        return res.send("Need resource & user");
-    }
+  } const result = lock(resource, user); res.send(result); 
+}); 
+// UNLOCK 
+app.get("/unlock", (req, res) => 
+    { const resource = req.query.resource;
+if (!resource) 
+    { return res.send("Need resource");
 
-    const result = lock(resource, user);
+ } 
+const result = unlock(resource);
+  
+ res.send(result); 
+}); 
 
-    res.send(result);
-
-});
-
-
-// UNLOCK
-app.get("/unlock", (req, res) => {
-
-    const resource = req.query.resource;
-
-    if (!resource) {
-        return res.send("Need resource");
-    }
-
-    const result = unlock(resource);
-
-    res.send(result);
+// SHOW LOCKS 
+app.get("/locks", (req, res) =>
+     { res.send(showLocks()); 
 
 });
-
-
-// SHOW LOCKS
-app.get("/locks", (req, res) => {
-    res.send(showLocks());
-});
-
 
 // SHOW QUEUE
-app.get("/queue", (req, res) => {
-    res.send(showQueue());
-});
+app.get("/queue", (req, res) => 
+    { res.send(showQueue());
+ });
 
+app.listen(3000, () => 
+    { console.log("Server started on port 3000"); 
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
-});
+  });
